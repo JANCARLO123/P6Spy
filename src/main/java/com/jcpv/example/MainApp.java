f6bda37717e3d8e8377301ae7978718b2a04dd83
Jan Carlo Perez <carlo.vallejo@citi.com.mx>
2017-06-15 13:11:21 -0500

    Inicia Proeyecto

---
+++ src/main/java/com/jcpv/example/MainApp.java
@@ -0,0 +1,7 @@
+package com.jcpv.example;
+
+/**
+ * Created by JanCarlo on 14/06/2017.
+ */
+public class MainApp {
+}
---
+++ src/main/java/com/jcpv/example/entity/Person.java
@@ -0,0 +1,44 @@
+package com.jcpv.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by JanCarlo on 14/06/2017.
+ */
+
+@Entity
+@Table(name = "PERSONS")
+public class Person {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    
+
+
+}
